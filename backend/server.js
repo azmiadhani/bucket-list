45bdc3a438022b6ab6e82a7ee2db131a478fec17
Azmi Adhani <azmiadhani@gmail.com>
2022-03-09 11:53:09 +0800

    404 handler

--- backend/server.js
+++ backend/server.js
@@ -23,4 +23,9 @@ app.use(routes);
 // asyncHandler gonna use errorHandler middleware that we made if there's an error thrown
 app.use(errorHandler);
 
+// Custom 404 route not found handler
+app.use((req, res) => {
+  res.status(404).send('404 not found');
+});
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
