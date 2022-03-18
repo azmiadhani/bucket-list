93f8f1b0f26b12b4b2965b0d74ba6f1fd673f7e8
Azmi Adhani <azmiadhani@gmail.com>
2022-03-18 15:39:22 +0800

    Header href using Link component

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,5 +1,5 @@
-import React from 'react';
 import './Header.css';
+import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header className="p-3 d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
@@ -11,24 +11,20 @@ const Header = () => {
       </a>
       <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
         <li>
-          <a href="#" className="nav-link px-2 link-secondary">
+          {/* To use Link comoponent, "to" attribute is what route the link pointing to */}
+          <Link to="/" className="nav-link px-2 link-secondary">
             Home
-          </a>
-        </li>
-        <li>
-          <a href="#" className="nav-link px-2 link-dark">
-            Features
-          </a>
+          </Link>
         </li>
       </ul>
 
       <div className="col-md-3 text-end">
-        <button type="button" className="btn btn-outline-primary me-2">
+        <Link to="/login" className="btn btn-outline-primary me-2">
           Login
-        </button>
-        <button type="button" className="btn btn-primary">
+        </Link>
+        <Link to="/register" className="btn btn-primary">
           Register
-        </button>
+        </Link>
       </div>
     </header>
   );
