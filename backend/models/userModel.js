d6826c3bfc173aeb70e92eee637f47e93fd6243b
Azmi Adhani <azmiadhani@gmail.com>
2022-03-19 17:17:39 +0800

    Login validation handling

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
 const bcrypt = require('bcrypt');
+const httpStatusCodes = require('../helpers/httpStatusCodesHelper');
+const { generateCustomApiError } = require('../helpers/customApiErrorHelper');
 
 const userSchema = mongoose.Schema(
   {
@@ -34,27 +36,35 @@ userSchema.pre('save', async function (next) {
 
 // static method to login user
 userSchema.statics.login = async function (email, password) {
+  var errorMessage = 'Sign in failed, please try again';
+  // input validation
+  if (!email || !password) {
+    let tempErrorObject = {};
+    if (!email) tempErrorObject.email = 'Please enter your email';
+    if (!password) tempErrorObject.password = 'Please enter your password';
+    throw generateCustomApiError(httpStatusCodes.BAD_REQUEST, errorMessage, {
+      ...tempErrorObject,
+    });
+    return;
+  }
   // check if email is valid to db
   const user = await this.findOne({ email });
-  if (user) {
-    // check entered password and db password
-    const auth = await bcrypt.compare(password, user.password);
-    if (auth) {
-      return user;
-    }
-    throw {
-      name: 'CustomApiError',
-      statusCode: 400,
-      message: 'Invalid password',
-      errors: {},
-    };
+  // if user not found
+  if (!user) {
+    throw generateCustomApiError(httpStatusCodes.BAD_REQUEST, errorMessage, {
+      email: 'Email not found',
+    });
+    return;
   }
-  throw {
-    name: 'CustomApiError',
-    statusCode: 400,
-    message: 'Invalid email',
-    errors: {},
-  };
+  // check entered password and db password
+  const auth = await bcrypt.compare(password, user.password);
+  if (auth) {
+    return user;
+  }
+  throw generateCustomApiError(httpStatusCodes.BAD_REQUEST, errorMessage, {
+    password: 'Invalid password',
+  });
+  return;
 };
 
 module.exports = mongoose.model('users', userSchema);
