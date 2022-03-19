70a24923ae8202aaa90f1a6ce6135ccfc1db4a9a
Azmi Adhani <azmiadhani@gmail.com>
2022-03-19 11:38:40 +0800

    Login & Register

--- frontend/src/pages/Auth/Login.js
+++ frontend/src/pages/Auth/Login.js
@@ -1,9 +1,55 @@
 import './Auth.css';
+import { useState, useContext } from 'react';
+import axios from '../../api/axios';
+import AuthContext from '../../context/AuthProvider';
 
 const Login = () => {
+  // AuthProvider state so that we can access it in this component
+  const { setAuth } = useContext(AuthContext);
+  // state management for form input
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  // state for error message
+  const [errorMessage, setErrorMessage] = useState(null);
+  // state for navigating
+  const [navigate, setNavigate] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // reset error message
+    setErrorMessage(null);
+    // send request to the api
+    axios
+      .post(
+        '/api/auth/login',
+        { email, password },
+        {
+          headers: { 'Content-Type': 'application/json' },
+        }
+      )
+      .then(function (response) {
+        // handle success
+        const accessToken = response?.data?.accessToken;
+        const refreshToken = response?.data?.refreshToken;
+        setAuth({ accessToken, refreshToken });
+        setNavigate(true);
+      })
+      .catch(function (error) {
+        // handle error
+        if (error.response.data.error.message) {
+          setErrorMessage(error.response.data.error.message);
+        }
+      })
+      .then(function () {
+        // always executed
+      });
+  };
+
+  // todo : handle navigate if login success
+
   return (
     <main className="form-signin">
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
 
         <div className="form-floating">
@@ -12,6 +58,8 @@ const Login = () => {
             className="form-control"
             id="floatingInput"
             placeholder="name@example.com"
+            required
+            onChange={(e) => setEmail(e.target.value)}
           />
           <label htmlFor="floatingInput">Email address</label>
         </div>
@@ -21,13 +69,18 @@ const Login = () => {
             className="form-control"
             id="floatingPassword"
             placeholder="Password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
           />
           <label htmlFor="floatingPassword">Password</label>
         </div>
 
-        <button className="w-100 btn btn-lg btn-primary" type="submit">
+        <button className="mt-4 w-100 btn btn-lg btn-primary" type="submit">
           Sign in
         </button>
+        {errorMessage && (
+          <p className="mt-2 error-message text-center">{errorMessage}</p>
+        )}
       </form>
     </main>
   );
--- frontend/src/pages/Auth/Register.js
+++ frontend/src/pages/Auth/Register.js
@@ -1,9 +1,56 @@
 import './Auth.css';
+import { useState } from 'react';
+import { Navigate } from 'react-router-dom';
+// axios importing
+import axios from '../../api/axios';
+const REGISTER_URL = '/api/auth/signup';
 
 const Register = () => {
+  // state management for form input
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [rePassword, setRePassword] = useState('');
+
+  const [navigate, setNavigate] = useState(false);
+
+  // handling form submit
+  const handleSubmit = async (e) => {
+    // prevent submit reload event
+    e.preventDefault();
+
+    // todo : check if password and re-password is same
+
+    // send request to the api
+    const response = await axios
+      .post(
+        REGISTER_URL,
+        { email, password },
+        {
+          headers: { 'Content-Type': 'application/json' },
+        }
+      )
+      .then(function (response) {
+        // handle success
+        const accessToken = response?.data?.accessToken;
+        const refreshToken = response?.data?.refreshToken;
+        setNavigate(true);
+      })
+      .catch(function (error) {
+        // todo : handle error
+      })
+      .then(function () {
+        // always executed
+      });
+  };
+
+  // if navigate is true, redirect to login page
+  if (navigate) {
+    return <Navigate to="/login" />;
+  }
+
   return (
     <main className="form-signin">
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1 className="h3 mb-3 fw-normal">Please sign up</h1>
 
         <div className="form-floating">
@@ -12,20 +59,33 @@ const Register = () => {
             className="form-control"
             id="floatingInput"
             placeholder="name@example.com"
+            onChange={(e) => setEmail(e.target.value)}
           />
           <label htmlFor="floatingInput">Email address</label>
         </div>
+
         <div className="form-floating">
           <input
             type="password"
             className="form-control"
             id="floatingPassword"
             placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
           />
           <label htmlFor="floatingPassword">Password</label>
         </div>
 
-        <button className="w-100 btn btn-lg btn-primary" type="submit">
+        <div className="form-floating">
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Re-type Password"
+            onChange={(e) => setRePassword(e.target.value)}
+          />
+          <label htmlFor="floatingRetypePassword">Re-type Password</label>
+        </div>
+
+        <button className="mt-4 w-100 btn btn-lg btn-primary" type="submit">
           Sign up
         </button>
       </form>
