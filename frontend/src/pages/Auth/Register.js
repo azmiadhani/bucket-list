b79ea47933c1400bd48534228e75700d6c6f7f56
Azmi Adhani <azmiadhani@gmail.com>
2022-03-19 16:50:14 +0800

    Register page updating

--- frontend/src/pages/Auth/Register.js
+++ frontend/src/pages/Auth/Register.js
@@ -10,7 +10,10 @@ const Register = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [rePassword, setRePassword] = useState('');
-
+  // state for error message
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [errorObject, setErrorObject] = useState(null);
+  // state for navigating
   const [navigate, setNavigate] = useState(false);
 
   // handling form submit
@@ -18,7 +21,16 @@ const Register = () => {
     // prevent submit reload event
     e.preventDefault();
 
-    // todo : check if password and re-password is same
+    var tempErrorObject = [];
+
+    // rePassword extra validation
+    if (!rePassword) {
+      tempErrorObject.rePassword = 'Please enter your re-typed password';
+    }
+    if (password !== rePassword) {
+      tempErrorObject.rePassword =
+        'Password and Re-type Password are not the same';
+    }
 
     // send request to the api
     const response = await axios
@@ -35,8 +47,19 @@ const Register = () => {
         const refreshToken = response?.data?.refreshToken;
         setNavigate(true);
       })
-      .catch(function (error) {
-        // todo : handle error
+      .catch(function (err) {
+        // handle error
+        let resErrorMessage = err?.response?.data?.error?.message;
+        let resErrorObject = err?.response?.data?.error?.errors;
+        setErrorMessage(resErrorMessage ? resErrorMessage : 'Unexpected Error');
+        console.log(tempErrorObject);
+        if (resErrorObject) {
+          Object.entries(resErrorObject).forEach(([key, value]) => {
+            tempErrorObject[key] = value;
+          });
+        }
+        console.log(tempErrorObject);
+        setErrorObject(tempErrorObject);
       })
       .then(function () {
         // always executed
@@ -54,40 +77,66 @@ const Register = () => {
         <h1 className="h3 mb-3 fw-normal">Please sign up</h1>
 
         <div className="form-floating">
+          {/* Input and Label*/}
           <input
             type="email"
-            className="form-control"
+            className={
+              'form-control' + (errorObject?.email ? ' is-invalid' : '')
+            }
             id="floatingInput"
             placeholder="name@example.com"
             onChange={(e) => setEmail(e.target.value)}
+            required
           />
           <label htmlFor="floatingInput">Email address</label>
+          {/* Validation text */}
+          <div className="invalid-feedback">
+            {errorObject?.email && errorObject?.email}
+          </div>
         </div>
 
         <div className="form-floating">
+          {/* Input and Label*/}
           <input
             type="password"
-            className="form-control"
+            className={
+              'form-control' + (errorObject?.password ? ' is-invalid' : '')
+            }
             id="floatingPassword"
             placeholder="Password"
             onChange={(e) => setPassword(e.target.value)}
+            required
           />
           <label htmlFor="floatingPassword">Password</label>
+          {/* Validation text */}
+          <div className="invalid-feedback">
+            {errorObject?.password && errorObject?.password}
+          </div>
         </div>
 
         <div className="form-floating">
           <input
             type="password"
-            className="form-control"
+            className={
+              'form-control' + (errorObject?.rePassword ? ' is-invalid' : '')
+            }
             placeholder="Re-type Password"
             onChange={(e) => setRePassword(e.target.value)}
+            required
           />
           <label htmlFor="floatingRetypePassword">Re-type Password</label>
+          <div className="invalid-feedback">
+            {errorObject?.rePassword && errorObject?.rePassword}
+          </div>
         </div>
 
+        {/* submit button */}
         <button className="mt-4 w-100 btn btn-lg btn-primary" type="submit">
           Sign up
         </button>
+        {errorMessage && (
+          <p className="mt-2 error-message text-center">{errorMessage}</p>
+        )}
       </form>
     </main>
   );
